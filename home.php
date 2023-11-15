5dd3a715f837ddc7d39ed06cf00456dfe60c4618
Ashish Labade (Vegan) <54700101+ashishvegan@users.noreply.github.com>
2023-11-15 11:17:51 +0530

    15 Nov 2023

--- home.php
+++ home.php
@@ -10,6 +10,7 @@
   $row = mysqli_fetch_array($sql);
   //echo var_dump($row);
   $name = $row['full_name'];
+  $nick = $row['nick_name'];
   $gender = $row['gender'];
   if($gender == "Male")
   {
@@ -25,8 +26,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <?php include("components/css-data.php");?>
     <title>Channel 51 | Home</title>
   </head>
   <body>
